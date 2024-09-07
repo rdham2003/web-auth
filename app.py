bd30dfc9b8f88341b9a70a5619413ddda4b2faff
Rohit Dham <rdhammpls@gmail.com>
2024-09-07 15:30:52 -0500

    edited slightly

--- app.py
+++ app.py
@@ -223,6 +223,6 @@ def decline():
     return render_template("console.html", requests=requests)
        
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
     
     
